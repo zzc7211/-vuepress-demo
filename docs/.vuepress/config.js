5c4dd4bcd5fe2c1ea5155437bf2cb4862f4cae52
zhouzichun <zhouzichun@xiaomi.com>
2022-01-24 21:45:11 +0800

    build

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    // 站点配置
+    base: "/vuepress-demo/",
+    lang: 'zh-CN',
+    title: '你好， VuePress ！',
+    description: '这是我的第一个 VuePress 站点',
+  
+    // 主题和它的配置
+    theme: '@vuepress/theme-default',
+    themeConfig: {
+      logo: 'https://vuejs.org/images/logo.png',
+    },
+  }
---
+++ docs/audio/README.md
@@ -0,0 +1,8 @@
+---
+lang: zh-CN
+title: audio总结
+description: audio总结
+---
+# 快速上手
+
+## audio 测试
